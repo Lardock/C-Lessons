1f35a912483a546db4899fd8b3761f227aa30a96
Pavel Lamzikov <Lamzikov@mail.ru>
2020-11-15 18:59:02 +0500

    Add files via upload

---
+++ Troelsen Pro C# 7/ConsoleChapter1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleGlav1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "MyConsole";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*******************************");
+            Console.WriteLine("Hello c#");
+            Console.WriteLine("*******************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadLine();
+            MessageBox.Show("Подключён!");
+        }
+    }
+}
