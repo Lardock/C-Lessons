78f6716b1ce2f6a2048ec09746dccd05b7f1ea1c
Pavel Lamzikov <Lamzikov@mail.ru>
2020-11-21 21:31:10 +0500

    Add files via upload

---
+++ Troelsen Pro C# 7/ConsoleChapter1/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleGlav1
+{
+    public class SportsCar : Car
+    {
+        public string GetPetName()
+        {
+            PetName = "Fred";
+            return PetName;
+        }
+    }
+}
